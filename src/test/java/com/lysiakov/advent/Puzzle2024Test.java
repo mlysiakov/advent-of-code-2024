b1f8a8f4b5c3c81479797a783ba4024f81ac7c70
Mykyta Lysiakov <mykyta.lysiakov@volvocars.com>
2024-12-03 15:31:35 +0100

    Make test abstracts to avoid creating class every time

---
+++ src/test/java/com/lysiakov/advent/Puzzle2024Test.java
@@ -0,0 +1,32 @@
+package com.lysiakov.advent;
+
+import com.lysiakov.advent.util.Puzzle;
+import com.lysiakov.advent.y2024.Day1;
+import com.lysiakov.advent.y2024.Day2;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Puzzle2024Test {
+
+  private Puzzle puzzle;
+
+  @ParameterizedTest
+  @MethodSource("test")
+  void test(Puzzle puzzle, long partOneExpected, long partTwoExpected) {
+    assertEquals(partOneExpected, puzzle.part1());
+    assertEquals(partTwoExpected, puzzle.part2());
+  }
+
+  private static Stream<Arguments> test() {
+    return Stream.of(
+        Arguments.of(new Day1(), 11, 31),
+        Arguments.of(new Day2(), 2, 4)
+    );
+  }
+
+}
